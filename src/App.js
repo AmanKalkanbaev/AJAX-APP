95590473dc9b58c02afc5cc357c2ab9ced88217d
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-03-31 01:38:13 +0600

    Created a new folder about AjAX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import Posts  from "./Posts/Posts.js";
+import Comments  from "./Comments/Comments.js";
+import Albums  from "./Albums/Albums.js";
+import Photos  from "./Photos/Photos.js";
+import Todos  from "./Todos/Todos.js";
+import Users  from "./Users/Users.js";
+
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Posts/>
+      <Comments/>
+      <Albums/>
+      <Photos/>
+      <Todos/>
+      <Users/>
     </div>
   );
 }
+
+
 
 export default App;
